3267b39a5a4f92d4abb2f0208be65dff843612e9
Jan Winkler <winkler@cs.uni-bremen.de>
2012-09-10 17:48:39 +0200

    Cleaned up package definition
    The package is now defined in :cl-user and the comments are cleaned up.

--- src/package.lisp
+++ src/package.lisp
@@ -1,19 +1,8 @@
-(desig-props:def-desig-package
- pr2-pick-and-place-scenario
+(in-package :cl-user)
+
+(desig-props:def-desig-package pr2-pick-and-place-scenario
  (:nicknames :pr2-pnp)
- (:use
-  #:location-costmap
-;;  #:cram-reasoning
-  #:cpl
-  #:roslisp
-;;  #:pr2-manipulation-process-module
-;;   #:cram-designators
-  #:cram-utilities
-;;   #:cram-process-modules
-  #:cram-roslisp-common
-  #:cram-plan-knowledge
-;;   #:cram-plan-failures
-  )
+ (:use #:location-costmap #:cl #:common-lisp #:crs #:cpl
+       #:roslisp #:cram-utilities
+       #:cram-roslisp-common #:cram-plan-knowledge)
  (:desig-props #:grasp))
-;;  #:alexandria))
-;; (:export #:handled-object-in-hand))
