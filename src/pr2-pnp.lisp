19c94aad87d6a59a64555badddcf995689a5d00f
Jan Winkler <winkler@cs.uni-bremen.de>
2012-09-11 16:28:24 +0200

    Moved them with-process-modules macro to the top of the file
    Lisp was complaining about the redefinition of with-process-modules. Fixed this by moving it before the top-level-plan.

--- src/pr2-pnp.lisp
+++ src/pr2-pnp.lisp
@@ -27,6 +27,14 @@
 
 (in-package :pr2-pnp)
 
+(defmacro with-process-modules (&body body)
+  `(cpm:with-process-modules-running
+       (pr2-manip-pm:pr2-manipulation-process-module
+        pr2-navigation-process-module:pr2-navigation-process-module
+        gazebo-perception-pm:gazebo-perception-process-module
+        point-head-process-module:point-head-process-module)
+     ,@body))
+
 (def-top-level-plan pick-and-place-scenario (object-name)
   ;; NOTE(winkler): Function changed. Now, the scenario takes an object identifier as it's only parameter. Atm, this only works with "mug", since later on, a mug designator is generated. This has nothing to do with the pose of the object, but wuth the handles on the object (this must be defined per-object). To be moved into knowledge base later.
   (with-process-modules
@@ -36,10 +44,11 @@
     ))
 
 (def-plan perceive-named-object (id)
-  (with-process-modules
+;  (with-process-modules
     (format t "Perceiving named object `~a'~%" id)
     (let ((obj (gazebo-perception-pm::make-named-object-designator id)))
-      (cram-plan-library:perceive-object 'cram-plan-library:a obj))))
+      (format t "test~%")
+      (cram-plan-library:perceive-object 'cram-plan-library:a obj)))
 
 (defun generate-mug-designator (&key (pose-stamped (gazebo-perception-pm::get-model-pose "mug")))
   (perception-pm::make-handled-object-designator
@@ -48,14 +57,6 @@
    :handles `((,(tf:make-pose (tf:make-3d-vector 0.13 0 0.06) (tf:euler->quaternion :ax (/ pi 2))) 0.01))))
 
 (defmethod cram-plan-knowledge:holds ((occasion t) &optional time) nil)
-
-(defmacro with-process-modules (&body body)
-  `(cpm:with-process-modules-running
-       (pr2-manip-pm:pr2-manipulation-process-module
-        pr2-navigation-process-module:pr2-navigation-process-module
-        gazebo-perception-pm:gazebo-perception-process-module
-        point-head-process-module:point-head-process-module)
-     ,@body))
 
 (defun get-latest-exectrace ()
   (cet:with-episode-knowledge cet:*last-episode-knowledge*
