dea92bbcf7283de789b376416dc2b8b184d941c5
Jan Winkler <winkler@cs.uni-bremen.de>
2012-10-18 15:29:05 +0200

    Added the grey pot to the scenario (two handled object)

--- src/object-database.lisp
+++ src/object-database.lisp
@@ -42,7 +42,25 @@
           (tf:make-3d-vector -1.0 -1.2 0.6)
           (tf:euler->quaternion :az (/ pi 4)))
    :file (model-path "mug.urdf")
-   :min-handles 1))
+   :min-handles 1)
+  (simple-knowledge::add-object-to-spawn
+   :name "cooking_pot"
+   :type :pot
+   :handles `((,(tf:make-pose
+                 (tf:make-3d-vector 0.175 0.0 0.12)
+                 (tf:make-identity-rotation))
+               0.01)
+              (,(tf:make-pose
+                 (tf:make-3d-vector 0.175 0.0 0.12)
+                 (tf:euler->quaternion :az pi))
+               0.01))
+   :pose (tf:make-pose-stamped
+          "map"
+          0.0
+          (tf:make-3d-vector -0.4 -1.5 0.6)
+          (tf:euler->quaternion :az -0.4))
+   :file (model-path "Cookingpot_2.urdf")
+   :min-handles 2))
   ;; (simple-knowledge::add-object-to-spawn
   ;;  :name "green_bottle"
   ;;  :type :bottle
